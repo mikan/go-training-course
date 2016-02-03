09c91eb1eb333fdc16d3f335dca7a1890c529c3e
mikan <yutakakato@icloud.com>
2016-02-03 22:42:51 +0900

    Updated ch01-ex03 and added ch01-ex07.

--- src/github.com/mikan/gopl/ch01/ex03/echo3.go
+++ src/github.com/mikan/gopl/ch01/ex03/echo3.go
@@ -5,12 +5,14 @@ package main
 import (
 	"fmt"
 	"strings"
+	"time"
 )
 func main() {
 	input := []string{"foo", "bar", "baz"}
-	run1(input)
-	run2(input)
-	// TODO: パフォーマンス計測コードを書く
+	secs1 := loop(1, 10000000, input)
+	secs2 := loop(2, 10000000, input)
+	fmt.Printf("run1: %.2fs\n", secs1);
+	fmt.Printf("run2: %.2fs\n", secs2);
 }
 
 func run1(count []string) {
@@ -19,9 +21,27 @@ func run1(count []string) {
 		s += sep + arg
 		sep = " "
 	}
-	fmt.Println(s)
+	// fmt.Println(s)
 }
 
 func run2(count []string) {
-	fmt.Println(strings.Join(count, " "))
+	// fmt.Println(strings.Join(count, " "))
+	strings.Join(count, " ")
 }
+
+func loop(method int, count int, input []string) float64 {
+	start := time.Now()
+	for i := 1; i < count; i++ {
+		switch method {
+		case 1: run1(input)
+		case 2: run2(input)
+		default: panic("unknown method")
+		}
+	}
+	return time.Since(start).Seconds()
+}
+
+// Result (Intel Xeon X3320 / Windows 10):
+// run1: 2.95s
+// run2: 2.16s
+
