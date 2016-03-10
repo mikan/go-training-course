b5547b86e2ebd3249743d40f4422533412cba9ef
mikan <yutakakato@icloud.com>
2016-03-10 20:23:04 +0900

    Fixed poster api call.

--- src/github.com/mikan/gopl/ch04/ex13/omdb/poster.go
+++ src/github.com/mikan/gopl/ch04/ex13/omdb/poster.go
@@ -12,13 +12,12 @@ import (
 func GetPoster(apiKey, id string) ([]byte, error) {
 	// Retrieve
 	url := strings.Replace(posterURL, apiKeyParam, apiKey, 1) + id
-	resp, err := http.Get(posterURL + id)
+	resp, err := http.Get(url)
 	if err != nil {
 		return nil, err
 	}
 	if resp.StatusCode != http.StatusOK {
 		resp.Body.Close()
-		fmt.Println("URL: " + url)
 		return nil, fmt.Errorf("search query failed: %s", resp.Status)
 	}
 
