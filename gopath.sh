3499822d52af002aaa14433a8d4a4fc8d234a127
KatoYutaka <yutaka@YKD6-Mac.mshome.net>
2016-06-11 15:55:09 +0900

    gipath.sh updated

--- gopath.sh
+++ gopath.sh
@@ -3,6 +3,12 @@
 # Update GOPATH by current directory.
 # Usage: source gopath.sh
 
-export GOPATH=$GOPATH:"$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)"
+current="$(cd "$(dirname "${BASH_SOURCE:-$0}")"; pwd)"
+
+if [ -z "$GOPATH" ]; then
+    export GOPATH=$current
+else
+    export GOPATH=$GOPATH:$current
+fi
 echo GOPATH updated to $GOPATH
 export PATH=$PATH:$GOPATH/bin
