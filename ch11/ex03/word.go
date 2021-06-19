4998b5ff180cce7cdb50fe8c1e57f5dc792b611c
mikan <mikan@aomikan.org>
2021-06-20 05:21:23 +0900

    update IsPalindrome

--- ch11/ex03/word.go
+++ ch11/ex03/word.go
@@ -1,10 +1,19 @@
 // Package word は、言葉遊び用のユーティリティを提供します。
 package word
 
+import "unicode"
+
 // IsPalindrome は s が前からでも後ろからでも同じように読めるかどうかを報告します。
+// 大文字小文字の違いは無視され、非文字も無視されます。
 func IsPalindrome(s string) bool {
-	for i := range s {
-		if s[i] != s[len(s)-1-i] {
+	var letters []rune
+	for _, r := range s {
+		if unicode.IsLetter(r) {
+			letters = append(letters, unicode.ToLower(r))
+		}
+	}
+	for i := range letters {
+		if letters[i] != letters[len(letters)-1-i] {
 			return false
 		}
 	}
